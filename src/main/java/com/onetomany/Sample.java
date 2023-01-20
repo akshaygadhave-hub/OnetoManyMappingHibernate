fa18e04927583bd3a13255b83da819f5bab57014
akshaygadhave <gadhaveakshay8@gmail.com>
2023-01-20 15:55:55 +0530

    data insert successfully

---
+++ src/main/java/com/onetomany/Sample.java
@@ -0,0 +1,46 @@
+package com.onetomany;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class Sample {
+
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+
+		Configuration configuration = new Configuration();
+		configuration.configure("hibernate.cfg.xml");
+
+		SessionFactory sf = configuration.buildSessionFactory();
+
+		Session s = sf.openSession();
+
+		Transaction t = s.beginTransaction();
+		
+		User user = new User();
+		Policy policy1 = new Policy("MK", "Health Insurance", "In-Active", user);
+		Policy policy2 = new Policy("RK", "Car Insurance", "Active", user);
+		
+		Set<Policy> set = new HashSet<Policy>();
+		set.add(policy1);
+		set.add(policy2);
+		
+		user.setPolicy(set);
+		user.setName("Nikhil");
+		user.setEmail("nikhil@rediffmail.com");
+		
+		s.save(user);
+		s.save(policy1);
+		s.save(policy2);
+		
+		t.commit();
+
+	}
+
+}
+
